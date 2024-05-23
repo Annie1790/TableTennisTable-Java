faa016e03b7d8e070365f6b036755bddef323674
Annie1790 <budayaniko@outlook.com>
2024-05-23 16:36:46 +0100

    added four new tests

--- app/src/test/java/tabletennistable/LeagueTest.java
+++ app/src/test/java/tabletennistable/LeagueTest.java
@@ -5,21 +5,27 @@ import org.junit.Assert;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class LeagueTest {
-    League league;
+    private final String BOB = "Bob";
+    private final String CHARLOTTE = "Charlotte";
+    private final String MICHELLE = "Michelle";
+    private League league;
+    private List<LeagueRow> rows;
 
     @BeforeEach
     public void setup() {
         //Given
-        league = new League();
+        rows = new ArrayList<LeagueRow>();
+        league = new League(rows);
     }
+
     @Test
-    public void testAddPlayer()
-    {
+    public void testAddPlayer() {
         // When
-        league.addPlayer("Bob");
+        league.addPlayer(BOB);
 
         // Then
         List<LeagueRow> rows = league.getRows();
@@ -31,19 +37,57 @@ public class LeagueTest {
 
     @Test
     public void playerIsNotAdded_IfPlayerAlreadyExist() {
-        league.addPlayer("Bob");
+        league.addPlayer(BOB);
 
         Assert.assertThrows(IllegalArgumentException.class, () -> {
-           league.addPlayer("Bob");
+            league.addPlayer(BOB);
         });
     }
 
     @Test
     public void playerIsNotAdded_IfNameContainsInvalidChars() {
+        final String BobWithCharacters = "Bob123/?";
+
         Assert.assertThrows(IllegalArgumentException.class, () -> {
-            league.addPlayer("Bob12´^&");
+            league.addPlayer(BobWithCharacters);
         });
     }
 
+    @Test
+    public void getWinnerReturnsPlayer() {
+        league.addPlayer(BOB);
+
+        Assert.assertTrue(league.getWinner().equals(BOB));
+    }
+
+    @Test
+    public void getWinnerWillReturnTopPlayer() {
+
+        league.addPlayer(BOB);
+        league.addPlayer(CHARLOTTE);
+        league.addPlayer(MICHELLE);
+
+        league.recordWin(CHARLOTTE, BOB);
+        league.recordWin(MICHELLE, CHARLOTTE);
+        league.recordWin(BOB, MICHELLE);
+
+        Assert.assertTrue(league.getWinner().equals(BOB));
+    }
+
+    @Test
+    public void getWinnerWillReturnNull_IfNoPlayersArePresent() {
+        Assert.assertNull(league.getWinner());
+    }
+
+    @Test
+    public void recordWinFail_IfWinnerIsAboveLoser() {
+        league.addPlayer(BOB);
+        league.addPlayer(CHARLOTTE);
+        league.addPlayer(MICHELLE);
+
+        Assert.assertThrows(IllegalArgumentException.class, () -> {
+            league.recordWin(BOB, MICHELLE);
+        });
+    }
 
 }
