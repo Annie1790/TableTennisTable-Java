e710fcc9f2b964a3262a108a09b0721a533b0618
Annie1790 <budayaniko@outlook.com>
2024-05-23 13:50:09 +0100

    added two new tests

--- app/src/test/java/tabletennistable/LeagueTest.java
+++ app/src/test/java/tabletennistable/LeagueTest.java
@@ -2,17 +2,22 @@ package tabletennistable;
 
 import org.hamcrest.core.IsCollectionContaining;
 import org.junit.Assert;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import java.util.List;
 
 public class LeagueTest {
+    League league;
+
+    @BeforeEach
+    public void setup() {
+        //Given
+        league = new League();
+    }
     @Test
     public void testAddPlayer()
     {
-        // Given
-        League league = new League();
-
         // When
         league.addPlayer("Bob");
 
@@ -23,4 +28,22 @@ public class LeagueTest {
         Assert.assertEquals(1, firstRowPlayers.size());
         Assert.assertThat(firstRowPlayers, IsCollectionContaining.hasItem("Bob"));
     }
+
+    @Test
+    public void playerIsNotAdded_IfPlayerAlreadyExist() {
+        league.addPlayer("Bob");
+
+        Assert.assertThrows(IllegalArgumentException.class, () -> {
+           league.addPlayer("Bob");
+        });
+    }
+
+    @Test
+    public void playerIsNotAdded_IfNameContainsInvalidChars() {
+        Assert.assertThrows(IllegalArgumentException.class, () -> {
+            league.addPlayer("Bob12´^&");
+        });
+    }
+
+
 }
