81504e7cfcfb277fc32a629204a99afbc35de6f4
AnikoBuday <aniko.t.buday@aexp.com>
2024-09-26 10:00:53 +0100

    main: setting up some test scenarios

--- app/src/test/java/tabletennistable/IntegrationTest.java
+++ app/src/test/java/tabletennistable/IntegrationTest.java
@@ -13,15 +13,35 @@ import java.io.InputStream;
 public class IntegrationTest {
 
     App app;
+    League league;
 
     @BeforeEach
     void setup() {
+        league = new Leauge();
+
         app = new App(new League(), new LeagueRenderer(), new FileService());
 
     }
     @Test
     public void testPrintsEmptyGame() {
         Assertions.assertEquals("No players yet", app.sendCommand("print"));
+    }
+    
+    @Test
+    public void shouldPrintUnknownCommand() {
+        final String COMMAND = "random command";
+
+        Assertions.assertEquals("Unknown command: " + command, app.sendCommand(COMMAND));
+    }
+
+    @Test
+    public void shouldLoadLeague_IfLeagueIsAlreadySaved() {
+
+    }
+
+    @Test
+    public void shouldThrowException_WhenLoading_AndLeagueIsNotSaved() {
+
     }
 
     @Test
@@ -31,8 +51,8 @@ public class IntegrationTest {
         boolean isGameActive = true;
         String[] args = null;
         final InputStream original = System.in;
-        final FileInputStream fips = new FileInputStream(new File("tabletennistable/Main.java"));
-        System.setIn(fips);
+        final FileInputStream inputStream = new FileInputStream(new File("tabletennistable/Main.java"));
+        System.setIn(inputStream);
         Main.main(args);
         System.setIn(original);
 
